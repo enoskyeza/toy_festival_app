3b67c599b457b5ab69483987a230a51d19f53456
enoskyeza <enoskyeza@gmail.com>
2023-11-02 15:42:35 +0300

    modified the templates

--- tf_app/register/urls.py
+++ tf_app/register/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
 from . import views
+from .views import ParentCreateView
+
+app_name = 'register'
 
 urlpatterns = [
     path('', views.home, name='home-page'),
-    path('', views.register, name='register-page'),
+    path('register/', ParentCreateView.as_view(), name='register-page'),
+    path('success/', views.success_page, name='success-page' )
 ]
