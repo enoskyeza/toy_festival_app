0f27a023d21d5afa444819ea28509cb7e12ac237
enoskyeza <enoskyeza@gmail.com>
2023-12-05 04:16:43 +0300

    Fix decorator user object bug

--- tf_app/utils/decorators.py
+++ tf_app/utils/decorators.py
@@ -1,18 +1,19 @@
 from functools import wraps
-
+from django.shortcuts import redirect
+from judges.models import Judge
 
 # Custom decorator to restrict judge page to judges
 def judge_required(view_func):
     @wraps(view_func)
     def _wrapped_view(request, *args, **kwargs):
         if request.user.is_authenticated:
-            if request.user.is_judge:  # Assuming is_judge is a boolean field in your Judge model
-                return view_func(request, *args, **kwargs)
-            else:
-                # If the user is logged in but not a judge, handle accordingly
-                return redirect('admin_dashboard:dashboard')  # Redirect to an error page or another view
-        else:
-            # If the user is not logged in, redirect to login page
-            return redirect('judge:judge-login')  # Redirect to your login page
+            try:
+                judge_profile = Judge.objects.get(user=request.user)
+                if judge_profile.is_judge:  # Assuming is_judge is a boolean field in your Judge model
+                    return view_func(request, *args, **kwargs)
+            except Judge.DoesNotExist:
+                pass  # Redirect or handle the case where the user isn't a judge
+        # If the user is not logged in or not a judge, handle accordingly
+        return redirect('not_judge_error')  # Redirect to an error page or another view
 
     return _wrapped_view
