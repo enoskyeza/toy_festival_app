6e7a0a5e3fe656ad534419fc6175c1a54cebc06e
enoskyeza <enoskyeza@gmail.com>
2023-12-05 03:23:32 +0300

    Pass login logic in judge login form

--- tf_app/judges/views.py
+++ tf_app/judges/views.py
@@ -1,25 +1,26 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+from .forms import JudgeLoginForm
 
 from register.models import Contestant
 from .models import Judge
 
 # Create your views here.
-def judge_login(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(username=username, password=password)
+# def judge_login(request):
+#     if request.method == 'POST':
+#         username = request.POST.get('username')
+#         password = request.POST.get('password')
+#         user = authenticate(username=username, password=password)
 
-        if user is not None and user.is_judge:
-            login(request, user)
-            return redirect('judging_page')
-        else:
-            # Invalid credentials or user is not a judge, handle accordingly
-            return render(request, 'judges/judge_login.html', {'error_message': 'Invalid login credentials'})
-    else:
-        return render(request, 'judges/judge_login.html')
+#         if user is not None and user.is_judge:
+#             login(request, user)
+#             return redirect('judging_page')
+#         else:
+#             # Invalid credentials or user is not a judge, handle accordingly
+#             return render(request, 'judges/judge_login.html', {'error_message': 'Invalid login credentials'})
+#     else:
+#         return render(request, 'judges/judge_login.html')
 
 
 @login_required
@@ -30,3 +31,24 @@ def judge_page(request):
 
     return render(request, 'judge_page.html', {'judge':judge, 'contenstants':contestants} )
 
+
+
+def judge_login(request):
+    if request.method == 'POST':
+        form = JudgeLoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+
+            if user is not None and user.is_judge:
+                login(request, user)
+                return redirect('judging_page')
+
+        # Invalid credentials or user is not a judge, handle accordingly
+        error_message = 'Invalid login credentials or user in not a judge'
+    else:
+        form = JudgeLoginForm()  # Create an instance of the form for GET requests
+        error_message = None
+
+    return render(request, 'judges/judge_login.html', {'form': form, 'error_message': error_message})
