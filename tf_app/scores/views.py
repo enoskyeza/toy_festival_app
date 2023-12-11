686a8563417dc34274c9eaad9a08b64dafe0e569
enoskyeza <enoskyeza@gmail.com>
2023-12-11 17:20:57 +0300

    Refactore score create view and template

--- tf_app/scores/views.py
+++ tf_app/scores/views.py
@@ -15,29 +15,112 @@ def submit_score(request, contestant_id):
     judge = Judge.objects.get(user=request.user)
 
     # criteria per category
-    criteria = JudgingCriteria.objects.all()
-    fun_criteria = JudgingCriteria.objects.filter(category__name='Fun')
-    function_criteria = JudgingCriteria.objects.filter(category__name='Function')
-    engineering_criteria = JudgingCriteria.objects.filter(category__name='Engineering and crafting')
-    creativity_criteria = JudgingCriteria.objects.filter(category__name='Creativity & Innovation')
+    criteria_by_category = {
+        'Fun': JudgingCriteria.objects.filter(category__name='Fun'),
+        'Function': JudgingCriteria.objects.filter(category__name='Function'),
+        'Engineering and crafting': JudgingCriteria.objects.filter(category__name='Engineering and crafting'),
+        'Creativity & Innovation': JudgingCriteria.objects.filter(category__name='Creativity & Innovation'),
+    }
 
     if request.method == 'POST':
-        for criterion in criteria:
-            score_value = request.POST.get(f'criteria_{criterion.id}')
-            score = Score.objects.create(contestant=contestant, criteria=criterion, score=score_value, judge=judge)
-            score.save()
+        for criterion_category, criteria_list in criteria_by_category.items():
+            for criterion in criteria_list:
+                score_value = request.POST.get(f'criteria_{criterion.id}')
+                score = Score.objects.create(contestant=contestant, criteria=criterion, score=score_value, judge=judge)
+                score.save()
         # Handle score submission
         return render(request, 'scores/submission_successful.html')
     return render(request, 'scores/submit_score.html', {
         'contestant': contestant,
-        'fun_criteria' : fun_criteria,
-        'function_criteria' : function_criteria,
-        'engineering_criteria' : engineering_criteria,
-        'creativity_criteria' : creativity_criteria,
-        })
+        'criteria_by_category': criteria_by_category,
+    })
+
+# @login_required
+# @judge_required
+# def update_score(request, contestant_id):
+#     contestant = get_object_or_404(Contestant, pk=contestant_id)
+#     judge = Judge.objects.get(user=request.user)
+#     scores = Score.objects.filter(judge=judge, contestant=contestant)
+
+#     # criteria per category
+#     criteria_by_category = {
+#         'Fun': list(JudgingCriteria.objects.filter(category__name='Fun')),
+#         'Function': list(JudgingCriteria.objects.filter(category__name='Function')),
+#         'Engineering and crafting': list(JudgingCriteria.objects.filter(category__name='Engineering and crafting')),
+#         'Creativity & Innovation': list(JudgingCriteria.objects.filter(category__name='Creativity & Innovation')),
+#     }
+
+
+#     if request.method == 'POST':
+#         for criterion_category, criteria_list in criteria_by_category.items():
+#             for criterion in criteria_list:
+#                 score_value = request.POST.get(f'criteria_{criterion.id}')
+#                 score_object = scores.filter(criteria=criterion).first()
+#             return render(request, 'scores/update_successful.html')
+
+#     return render(request, 'scores/update_score.html', {
+#         'contestant': contestant,
+#         'criteria_by_category': criteria_by_category,
+#         'scores': scores,
+#     })
+#     # criteria = JudgingCriteria.objects.all()
+#     # fun_criteria = JudgingCriteria.objects.filter(category__name='Fun')
+#     # function_criteria = JudgingCriteria.objects.filter(category__name='Function')
+#     # engineering_criteria = JudgingCriteria.objects.filter(category__name='Engineering and crafting')
+#     # creativity_criteria = JudgingCriteria.objects.filter(category__name='Creativity & Innovation')
+
+#     if request.method == 'POST':
+#         for criterion in criteria:
+#             for score_object in scores:
+#                 if criterion == score_object.criteria:
+#                     score_value = request.POST.get(f'criteria_{criterion.id}')
+#                     score_object.score = score_value
+#                     score_object.save()
+#         # Handle score submission
+#         return render(request, 'scores/submission_successful.html')
+#     return render(request, 'scores/submit_score.html', {
+#         'contestant': contestant,
+#         'fun_criteria' : fun_criteria,
+#         'function_criteria' : function_criteria,
+#         'engineering_criteria' : engineering_criteria,
+#         'creativity_criteria' : creativity_criteria,
+#         'scores': scores,
+#         })
 
 @login_required
 def contestant_scores(request, contestant_id):
     contestant = get_object_or_404(Contestant, pk=contestant_id)
     scores = Score.objects.filter(contestant=contestant)
     return render(request, 'scores/contestant_score_details.html', {'contestant': contestant, 'scores': scores})
+
+
+
+
+# Refactored code blocks
+
+# Create view for scores
+# def submit_score(request, contestant_id):
+#     contestant = get_object_or_404(Contestant, pk=contestant_id)
+#     judge = Judge.objects.get(user=request.user)
+
+#     # criteria per category
+#     criteria = JudgingCriteria.objects.all()
+#     fun_criteria = JudgingCriteria.objects.filter(category__name='Fun')
+#     function_criteria = JudgingCriteria.objects.filter(category__name='Function')
+#     engineering_criteria = JudgingCriteria.objects.filter(category__name='Engineering and crafting')
+#     creativity_criteria = JudgingCriteria.objects.filter(category__name='Creativity & Innovation')
+
+#     if request.method == 'POST':
+#         for criterion in criteria:
+#             score_value = request.POST.get(f'criteria_{criterion.id}')
+#             score = Score.objects.create(contestant=contestant, criteria=criterion, score=score_value, judge=judge)
+#             score.save()
+#         # Handle score submission
+#         return render(request, 'scores/submission_successful.html')
+#     return render(request, 'scores/submit_score.html', {
+#         'contestant': contestant,
+#         'fun_criteria' : fun_criteria,
+#         'function_criteria' : function_criteria,
+#         'engineering_criteria' : engineering_criteria,
+#         'creativity_criteria' : creativity_criteria,
+#         })
