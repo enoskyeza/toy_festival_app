0a8eed42b6f9473f127410a669750f074e1fcd25
enoskyeza <enoskyeza@gmail.com>
2023-12-12 21:52:16 +0300

    Add script file for filtering

---
+++ tf_app/judges/static/judges/script.js
@@ -0,0 +1,15 @@
+const inputEl = document.getElementById('search-input')
+const allBtn = document.getElementById('all-btn')
+const contestantSection = document.getElementById('contestant-section-id')
+
+allBtn.addEventListener('click', function() {
+    console.log('All button clicked')
+})
+
+console.log('Script is loaded... ')
+
+inputEl.addEventListener('input', function() {
+
+})
+
+console.log(contestantSection)
