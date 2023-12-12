bb2aab038f9029e32d6b281cb2b18f2369dd8d1a
enoskyeza <enoskyeza@gmail.com>
2023-12-12 22:53:26 +0300

    Test logout feature

--- tf_app/tf_app/settings.py
+++ tf_app/tf_app/settings.py
@@ -130,3 +130,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# settings.py
+LOGOUT_REDIRECT_URL = '/accounts/login/'
