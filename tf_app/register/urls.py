f6b1338667203ef60bc98cd0f0f97646ce4512c8
enoskyeza <enoskyeza@gmail.com>
2023-12-13 00:10:06 +0300

    Add contestant list view

--- tf_app/register/urls.py
+++ tf_app/register/urls.py
@@ -8,5 +8,6 @@ app_name = 'register'
 urlpatterns = [
     path('', views.home, name='home-page'),
     path('register/', RegistrationView.as_view(), name='register-page'),
-    path('success/<int:contestant_id>/', views.success_page, name='success-page' )
+    path('success/<int:contestant_id>/', views.success_page, name='success-page' ),
+    path('list/', views.contestant_list_view, name='contestant-list' ),
 ]
