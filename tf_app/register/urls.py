f879e02b00d6080638ceaa618fd082e870544d7d
enoskyeza <enoskyeza@gmail.com>
2023-12-15 09:10:52 +0300

    fix url bug

--- tf_app/register/urls.py
+++ tf_app/register/urls.py
@@ -7,7 +7,7 @@ app_name = 'register'
 
 urlpatterns = [
     path('', views.home, name='home-page'),
-    path('register/', RegistrationView.as_view(), name='register-page'),
+    path('reg/', RegistrationView.as_view(), name='register-page'),
     path('success/<int:contestant_id>/', views.success_page, name='success-page' ),
     path('list/', views.contestant_list_view, name='contestant-list' ),
 ]
