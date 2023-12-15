68cdc40978d2de81779c4df281499de3170b0e98
enoskyeza <enoskyeza@gmail.com>
2023-12-15 08:44:03 +0300

    Add Users and implement login

--- tf_app/admin_dashboard/forms.py
+++ tf_app/admin_dashboard/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 
-class JudgeLoginForm(forms.Form):
+class UserLoginForm(forms.Form):
     username = forms.CharField(label='Username')
     password = forms.CharField(label='Password', widget=forms.PasswordInput)
-    # remember_me = forms.BooleanField(label='Remember me', required=False)
     # Add other fields or customizations as needed
--- tf_app/admin_dashboard/views.py
+++ tf_app/admin_dashboard/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+
+from .forms import UserLoginForm
 
 from register.models import Contestant
 
@@ -11,18 +14,43 @@ from register.models import Contestant
 
 @login_required
 def index(request):
-    students = Contestant.objects.all()
-    return render(request, "admin_dashboard/dashboard.html", context={'students': students})
+    contestants = Contestant.objects.all()
+    user = request.user
+    return render(request, "admin_dashboard/dashboard.html", context={'contestants': contestants,
+                                                                      'user':user,})
+
+
+# def custom_login(request):
+#     form = UserLoginForm(request.POST)
+
+#     if request.method == 'POST':
+#         username = request.POST.get('username')
+#         password = request.POST.get('password')
+#         user = authenticate(request, username=username, password=password)
+#         if user is not None:
+#             login(request, user)
+#             return HttpResponseRedirect(reverse('dashboard'))
+#         else:
+#             return render(request, 'admin_dashboard/login.html', {'error_message': 'Invalid credentials'})
+#     else:
+#         return render(request, 'admin_dashboard/login.html')
 
 def custom_login(request):
+    if request.user.is_authenticated:
+        # If the user is already authenticated, redirect them to the dashboard
+        return HttpResponseRedirect(reverse('admin_dashboard:dashboard'))
+
     if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return HttpResponseRedirect(reverse('dashboard'))
-        else:
-            return render(request, 'admin_dashboard/login.html', {'error_message': 'Invalid credentials'})
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect(reverse('admin_dashboard:dashboard'))
+        # If authentication fails or form data is invalid, render the login page with an error message
+        return render(request, 'admin_dashboard/login.html', {'form': form, 'error_message': 'Invalid credentials'})
     else:
-        return render(request, 'admin_dashboard/login.html')
+        form = UserLoginForm()
+        return render(request, 'admin_dashboard/login.html', {'form': form})
