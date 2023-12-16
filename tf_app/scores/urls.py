1bd9837034d3108c80321549c9000a337562c7e5
enoskyeza <enoskyeza@gmail.com>
2023-12-16 15:17:04 +0300

    scores pages

--- tf_app/scores/urls.py
+++ tf_app/scores/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('comment/<int:contestant_id>', views.judge_comment, name='comment'),
     path('details/<int:contestant_id>', views.contestant_scores, name='judge-scores'),
     path('event/', views.event_scores, name='event-scores'),
+    path('overall/', views.overall_scores, name='overall-scores'),
 
     # path('success', views.contestant_scores, name='success'),
 ]
