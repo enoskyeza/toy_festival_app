05838100ee2dce1adce899435383641e6c296615
enoskyeza <enoskyeza@gmail.com>
2024-05-08 06:35:16 +0300

    Add blog views

--- tf_app/blog/views.py
+++ tf_app/blog/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import AuthorSerializer, PostSerializer, CommentSerializer
 
-# Create your views here.
+from .models import Author, Post, Comment
+
+
+# Define ViewSets for each model
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
