104c521616e6d3f0221e90f3af4ad8acd374925f
enoskyeza <enoskyeza@gmail.com>
2024-05-08 05:04:23 +0300

    Add post author

--- tf_app/blog/models.py
+++ tf_app/blog/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Author(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    pass
+    image = models.ImageField(blank=True)
 
 
 class Post(models.Model):
@@ -14,8 +14,8 @@ class Post(models.Model):
     like = models.IntegerField(default=0)
     title = models.CharField(max_length=200)
     copy = models.TextField()
-    subcopy = models.TextField()
-    image = models.ImageField()
+    subcopy = models.TextField(blank=True)
+    image = models.ImageField(blank=True)
 
     def like_post(self):
         """
@@ -24,25 +24,3 @@ class Post(models.Model):
         self.like += 1
         self.save()
 
-# class Comments(models.Model):
-#     created = models.DateTimeField(auto_now_add=True)
-#     comment = models.CharField()
-#     user = models.CharField()
-#
-# class Likes(models.Model):
-#     number = models.IntegerField()
-#
-# class Author(models.Model):
-#     user = models.OneToOneField(User)
-#     name = models.CharField()
-#     image = models.ImageField()
-# class Post(models.Model):
-#     created = models.DateTimeField(auto_now_add=True)
-#     author = models.ForeignKey(Author)
-#     tag = models.CharField()
-#     title = models.TextField()
-#     copy = models.TextField()
-#     subcopy = models.TextField()
-#     comments = models.ManyToOneRel(Comments)
-#     image = models.ImageField()
-#
