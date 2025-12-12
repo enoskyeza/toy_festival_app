3a781419c76ead042ace37e5114e9b28002aa339
enoskyeza <enoskyeza@gmail.com>
2025-12-12 11:41:37 +0300

    add judge management views

--- tf_app/accounts/urls.py
+++ tf_app/accounts/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
-from .views import LoginView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import LoginView, JudgeViewSet
+
+router = DefaultRouter()
+router.register(r'judges', JudgeViewSet, basename='judge')
 
 urlpatterns = [
     path('', LoginView.as_view(), name='login'),
+    path('', include(router.urls)),
     # path('logout/', LogoutView.as_view(), name='logout'),
 ]
